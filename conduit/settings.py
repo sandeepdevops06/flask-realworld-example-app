5cdde99d864a7ee2e9f3c1b65673ee4be139ee87
sandeepdevops06 <97302723+sandeepdevops06@users.noreply.github.com>
2022-05-05 14:07:29 +0530

    Update settings.py

--- conduit/settings.py
+++ conduit/settings.py
@@ -7,37 +7,7 @@ from datetime import timedelta
 class Config(object):
     """Base configuration."""
 
-    SECRET_KEY = os.environ.get('CONDUIT_SECRET', 'secret-key')  # 
-# These are here so you can change them to customize the program
-# easily.
-default_greeting = "Hello World!"
-filename = "greeting.txt"
-
-
-import sys
-
-def askyesno(question):
-    while True:
-        answer = input(question + ' (y or n) ')
-        if answer == 'Y' or answer == 'y':
-            return True
-        if answer == 'N' or answer == 'n':
-            return False
-
-def greet():
-    with open(filename, 'r') as f:
-        for line in f:
-            print(line.rstrip('\n'))
-
-try:
-    greet()
-except OSError:
-    print("Cannot read '%s'!" % filename, file=sys.stderr)
-    if askyesno("Would you like to create a default greeting file?"):
-        with open(filename, 'w') as f:
-            print(default_greeting, file=f)
-        greet()
-        
+    SECRET_KEY = os.environ.get('CONDUIT_SECRET', 'secret-key')  # TODO change me     
     APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     BCRYPT_LOG_ROUNDS = 13
