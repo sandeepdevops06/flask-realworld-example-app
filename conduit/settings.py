dfe1e1d50bb6db97ed8edcb98da854a82f2a2943
sandeepdevops06 <97302723+sandeepdevops06@users.noreply.github.com>
2022-05-05 13:29:08 +0530

    Update settings.py

--- conduit/settings.py
+++ conduit/settings.py
@@ -7,8 +7,8 @@ from datetime import timedelta
 class Config(object):
     """Base configuration."""
 
-    SECRET_KEY = os.environ.get('CONDUIT_SECRET', 'secret-key')  # 
-    APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
+   // SECRET_KEY = os.environ.get('CONDUIT_SECRET', 'secret-key')  # 
+    //APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     BCRYPT_LOG_ROUNDS = 13
     DEBUG_TB_INTERCEPT_REDIRECTS = False
