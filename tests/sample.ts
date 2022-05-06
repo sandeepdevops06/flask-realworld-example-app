f440e1efbe709a371f8aadf492e3a6a03a44902e
sandeepdevops06 <97302723+sandeepdevops06@users.noreply.github.com>
2022-05-06 15:49:24 +0530

    Update sample.ts

--- tests/sample.ts
+++ tests/sample.ts
@@ -29,6 +29,24 @@ const OPENVINO_URL = 'https://docs.openvino.ai/latest/index.html';
 const DEFAULT_AWS_S3_REGIONS: string[][] = [
     ['us-east-1', 'US East (N. Virginia)'],
     ['us-east-2', 'US East (Ohio)'],
+    /** IE9, IE10 and IE11 requires all of the following polyfills. **/
+// import 'core-js/es6/symbol';
+// import 'core-js/es6/object';
+// import 'core-js/es6/function';
+// import 'core-js/es6/parse-int';
+// import 'core-js/es6/parse-float';
+// import 'core-js/es6/number';
+// import 'core-js/es6/math';
+// import 'core-js/es6/string';
+// import 'core-js/es6/date';
+// import 'core-js/es6/array';
+// import 'core-js/es6/regexp';
+// import 'core-js/es6/map';
+// import 'core-js/es6/weak-map';
+// import 'core-js/es6/set';
+
+/** IE10 and IE11 requires the following for NgClass support on SVG elements */
+// import 'classlist.js';  // Run `npm install --save classlist.js`.
     ['us-west-1', 'US West (N. California)'],
     ['us-west-2', 'US West (Oregon)'],
     ['ap-south-1', 'Asia Pacific (Mumbai)'],
